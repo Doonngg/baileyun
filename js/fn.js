731b855d0e8eab7c5aba67f2c2a4bcc205129181
Doonngg <61999334+Doonngg@users.noreply.github.com>
2022-03-03 16:39:27 +0800

    Add files via upload

---
+++ js/fn.js
@@ -0,0 +1,34 @@
+/*窗口大小改变，切换图片路径*/
+window.onload=function(){
+   if(document.body.clientWidth<600){
+   	/*手机版路径*/
+   	$(".banner").attr("src","img/bannerm.png");
+   } else{
+   	/*电脑版路径*/
+   	$(".banner").attr("src","img/banner1.png")
+   }
+}
+window.onresize = function(){
+	console.log(document.body.clientWidth);
+	if(document.body.clientWidth<600){
+		/*手机版路径*/
+		$(".banner").attr("src","img/bannerm.png");
+	} else{
+		/*电脑版路径*/
+		$(".banner").attr("src","img/banner1.png")
+	}
+}
+//点击展开头部列表
+$(".fold").click(function(){
+	myAnimate(".fold-page", "100%", "height", "height .3s");
+})
+$(".fold-close").click(function(){
+	myAnimate(".fold-page", "0", "height", "height .3s");
+})
+
+
+//css3动画封装
+	function myAnimate(animateNode, animateEnd, animateStyle, animateTransition) {
+		document.querySelector(animateNode).style.transition = animateTransition;
+		document.querySelector(animateNode).style[animateStyle] = animateEnd;
+	}
