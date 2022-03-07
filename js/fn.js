298fe26582cef8da940c3f4d4eae3bcbd9b6eed5
Doonngg <61999334+Doonngg@users.noreply.github.com>
2022-03-07 15:48:12 +0800

    Add files via upload

--- js/fn.js
+++ js/fn.js
@@ -32,3 +32,109 @@ $(".fold-close").click(function(){
 		document.querySelector(animateNode).style.transition = animateTransition;
 		document.querySelector(animateNode).style[animateStyle] = animateEnd;
 	}
+	
+	
+	
+	
+//地图部分
+var myChart = echarts.init(document.getElementById('china-map'));
+        var geoCoordMap = {
+           "青岛": [120.33, 36.07],
+           "临沂": [118.35, 35.05],
+           "苏州": [120.62, 31.32]
+        };
+
+        var convertData = function (data) {
+            var res = [];
+            for (var i = 0; i < data.length; i++) {
+                var geoCoord = geoCoordMap[data[i].name];
+                if (geoCoord) {
+                    res.push(geoCoord.concat(data[i].value));
+                }
+            }
+            return res;
+        };
+
+        option = {
+            backgroundColor: '#FFFFFF',
+            title: {
+                text: '',
+                subtext: '',
+                left: 'center',
+                textStyle: {
+                    color: '#fff'
+                }
+            },
+            tooltip: {
+                trigger: 'item',
+				formatter: function(params) {  
+				//鼠标经过浮窗显示内容
+					return params.data[2];
+            }  
+            },
+            legend: {
+                orient: 'vertical',
+                top: 'bottom',
+                left: 'right',
+                data: [],
+                textStyle: {
+                    color: '#fff'
+                },
+            },
+            visualMap: {
+				show:false
+            },
+            geo: {
+                map: 'china',
+                label: {
+                    emphasis: {
+                        show: true
+                    }
+                },
+                itemStyle: {
+                    normal: {
+                        areaColor: '#ED6C00',
+                        borderColor: '#FFEBDB'
+                    },
+                    emphasis: {
+                        areaColor: '#FFC888'
+                    }
+                }
+            },
+            series: [
+                {
+                    name: '门店',
+                    type: 'scatter',
+                    coordinateSystem: 'geo',
+                    data: convertData([
+                        { name: "苏州", value: "苏州百乐运体验店" },
+                        { name: "青岛", value: "青岛百乐运体验店" },
+                        { name: "临沂", value: "临沂百乐运体验店" }
+                    ]),
+                    symbolSize: 8,
+                    label: {
+                        normal: {
+                            show: false
+                        },
+                        emphasis: {
+                            show: false
+                        }
+                    },
+                    itemStyle: {
+                        emphasis: {
+                            borderColor: '',
+                            borderWidth: 1
+                        }
+                    }
+                }
+            ]
+        }
+        myChart.setOption(option);
+		//地图点击事件
+		myChart.on("click",function(data){
+			if(data.name == "山东"){
+				console.log("当前点击省份为"+data.name);
+			}
+			console.log(data.data[2]);
+			console.log(data);
+		});
